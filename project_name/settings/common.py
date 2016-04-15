cb6a6bdd56cdd94825670279472b3294bb569037
tudouya <568865013@qq.com>
2016-04-15 11:04:14 +0800

    Update:更新配置文件

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -1,58 +1,51 @@
-# -*- coding: utf-8 -*-
+"""
+Django settings for {{ project_name }} project.
 
-# Import sys (to adjust Python path)
-# import sys
-# Import some utility functions
-from os.path import abspath, basename, dirname, join, normpath
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
 
-# #########################################################
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+import os
+import sys
 
-# ##### PATH CONFIGURATION ################################
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-# Fetch Django's project directory
-DJANGO_ROOT = dirname(dirname(abspath(__file__)))
+# App/Library Paths
+sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
-# Fetch the project_root
-PROJECT_ROOT = dirname(DJANGO_ROOT)
-
-# The name of the whole site
-SITE_NAME = basename(DJANGO_ROOT)
-
-# Collect static files here
-STATIC_ROOT = join(PROJECT_ROOT, 'run', 'static')
-
-# Collect media files here
-MEDIA_ROOT = join(PROJECT_ROOT, 'run', 'media')
-
-# look for static assets here
-STATICFILES_DIRS = [
-    join(PROJECT_ROOT, 'static'),
-]
-
-# look for templates here
-# This is an internal setting, used in the TEMPLATES directive
-PROJECT_TEMPLATES = [
-    join(PROJECT_ROOT, 'templates'),
-]
-
-# Add apps/ to the Python path
-# sys.path.append(normpath(join(PROJECT_ROOT, 'apps')))
+# Users TO EMAIL Errors to based on LOGGING Settings
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+MANAGERS = ADMINS
 
 
-# ##### APPLICATION CONFIGURATION #########################
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '{{ secret_key }}'
 
-# This are the apps
-DEFAULT_APPS = [
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# DATABASE Configured by URL
+# import dj_database_url
+# DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+
+
+INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+)
 
-# Middlewares
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -60,95 +53,115 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-]
-
-# Template stuff
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': PROJECT_TEMPLATES,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages'
-            ],
-        },
-    },
-]
-
-
-# ##### SECURITY CONFIGURATION ############################
-
-# We store the secret key here
-# The required SECRET_KEY is fetched at the end of this file
-SECRET_FILE = normpath(join(PROJECT_ROOT, 'run', 'SECRET.key'))
-
-# These persons receive error notification
-ADMINS = (
-    ('your name', 'your_name@example.com'),
 )
-MANAGERS = ADMINS
+
+ROOT_URLCONF = '{{ project_name }}.urls'
+
+WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 
 
-# ##### DJANGO RUNNING CONFIGURATION ######################
-
-# The default WSGI application
-WSGI_APPLICATION = '%s.wsgi.application' % SITE_NAME
-
-# The root URL configuration
-ROOT_URLCONF = '%s.urls' % SITE_NAME
-
-# This site's ID
-SITE_ID = 1
-
-# The URL for static files
-STATIC_URL = '/static/'
-
-# The URL for media files
-MEDIA_URL = '/media/'
-
-
-# ##### DEBUG CONFIGURATION ###############################
-DEBUG = False
-
-
-# ##### INTERNATIONALIZATION ##############################
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
 TIME_ZONE = 'Asia/Shanghai'
-USE_TZ = True
 
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'zh-hans'
 LANGUAGES = (
     ('en', (u'English')),
     ('zh-cn', (u'简体中文')),
 )
 
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = False
 
-# Internationalization
-USE_I18N = True
-
-# Localisation
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
 USE_L10N = True
 
-# enable timezone awareness by default
+# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
 
-# Finally grab the SECRET KEY
-try:
-    SECRET_KEY = open(SECRET_FILE).read().strip()
-except IOError:
-    try:
-        from django.utils.crypto import get_random_string
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!$%&()=+-_'
-        SECRET_KEY = get_random_string(50, chars)
-        with open(SECRET_FILE, 'w') as f:
-            f.write(SECRET_KEY)
-    except IOError:
-        raise Exception('Could not open %s for writing!' % SECRET_FILE)
+# Upload Media
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'uploads')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/uploads/'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Template Settings
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -1,2 +1,8 @@
 # for now fetch the development settings only
 from dev import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
+
+# ALLOWED_HOSTS = ['*']
